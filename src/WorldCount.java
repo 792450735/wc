0f2fafc523e7d31bb5b03d291056f12028cfdefa
limincong <792450735@qq.com>
2018-03-18 12:12:26 +0800

    完成扩展功能

--- src/WorldCount.java
+++ src/WorldCount.java
@@ -1,107 +1,176 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class WorldCount {
-	public static int words=1;
+	public static int words=0;
 	public static int lines=1;
-	public static int chars=0;
-	public static String fname;
+	public static int chars=0,t1=0,t2=0,t3=0;
+	public static ArrayList<String> fname=new ArrayList<String>();
 	public static String outname;
+	public static String stopList;
+	
+	public static void init(){
+		words=1;
+		lines=1;
+		chars=0;
+		t1=0;
+		t2=0;
+		t3=0;
+	}
 	
 	public static void wc(String ff,int[] use) throws IOException{
 		int c=0;
 		String fff=new String("src/"+ff);
 		InputStream f=new FileInputStream(ff);////
 		boolean lastNotWhite=false;
+		boolean lastisword=false;
 		String whiteSpace=" \t\n\r,";
-		while((c=f.read())!=-1){
-			chars++;
-			if(c=='\n'){
-				lines++;
-				chars=chars-2;
+		if(use[3]==1){
+			StringBuilder list= new StringBuilder();
+			StringBuilder newlist= new StringBuilder();
+			ArrayList<String> stopwords = new ArrayList<String>();
+			InputStream fs=new FileInputStream(stopList);
+			while((c=fs.read())!=-1){
+				if(whiteSpace.indexOf(c)==-1){
+					list.append((char)c);
+				}
+				if(whiteSpace.indexOf(c)!=-1){
+					if(lastNotWhite){
+						stopwords.add(list.toString());
+						list.delete(0, list.length());
+						
+					}
+					lastisword=true;
+					lastNotWhite=false;
+				}
+				else{
+					lastisword=false;
+					lastNotWhite=true;
+				}
 			}
-			if(whiteSpace.indexOf(c)!=-1){
-				if(lastNotWhite)
-					words++;
-				lastNotWhite=false;
+			if(!lastisword){
+				stopwords.add(list.toString());
 			}
-			else
-				lastNotWhite=true;
+			lastisword=false;
+			lastNotWhite=false;
+			while((c=f.read())!=-1){
+				chars++;
+				if(c=='\n'){
+					lines++;
+					chars=chars-2;
+				}
+				if(whiteSpace.indexOf(c)==-1){
+					newlist.append((char)c);
+				}
+				if(whiteSpace.indexOf(c)!=-1){
+					if(lastNotWhite){
+						words++;
+						for(int j=0;j<stopwords.size();j++){
+							if(newlist.toString().equals(stopwords.get(j)))
+								words--;
+						}
+						newlist.delete(0, newlist.length());
+					}
+					lastNotWhite=false;
+					lastisword=true;
+				}
+				else{
+					lastisword=false;
+					lastNotWhite=true;
+				}
+			}
+			if(!lastisword){
+				for(int j=0;j<stopwords.size();j++){
+					if(newlist.toString().equals(stopwords.get(j)))
+						words--;
+				}
+			}
 		}
+		else{
+			while((c=f.read())!=-1){
+				chars++;
+				if(c=='\n'){
+					lines++;
+					chars=chars-2;
+				}
+				if(whiteSpace.indexOf(c)!=-1){
+					if(lastNotWhite)
+						words++;
+					lastNotWhite=false;
+					lastisword=true;
+				}
+				else{
+					lastisword=false;
+					lastNotWhite=true;
+				}
+			}
+		}
+		
 		for(int i=0;i<use.length;i++){
 			if(use[i]==1){
 				switch(i){
-				case 0:System.out.println(fname+",字符数："+chars);break;
-				case 1:System.out.println(fname+",单词数："+words);break;
-				case 2:System.out.println(fname+",行数："+lines);break;
+				case 0:System.out.println(ff+",字符数："+chars);break;
+				case 1:System.out.println(ff+",单词数："+words);break;
+				case 2:System.out.println(ff+",行数："+lines);break;
 				default:
 				}
 			}
 		}
 	}
 	
-	public static void oout(String f,int[] use) throws IOException{
+	public static void oout(String f,String ff,int[] use) throws IOException{
 		File file = new File(f);
-		if(!file.exists()){
-		       try {
-		            if(file.createNewFile()){
-		             }else{
-		                 System.out.println("文件创建失败！");
-		             }
-		         } catch (IOException e) {
-		               e.printStackTrace();
-		         }
-		}
-        //2：准备输出流
-        FileWriter out = new FileWriter(file);
-		for(int i=0;i<use.length;i++){
-			if(use[i]==1){
-				switch(i){
-				case 0:out.write(fname+",字符数："+chars+"\r\n");break;
-				case 1:out.write(fname+",单词数："+words+"\r\n");break;
-				case 2:out.write(fname+",行数："+lines+"\r\n");break;
-				default:
+		
+			//2：准备输出流
+			FileWriter out = new FileWriter(file,true);
+			for(int i=0;i<use.length;i++){
+				if(use[i]==1){
+					switch(i){
+					case 0:out.write(ff+",字符数："+chars+"\r\n");break;
+					case 1:out.write(ff+",单词数："+words+"\r\n");break;
+					case 2:out.write(ff+",行数："+lines+"\r\n");break;
+					case 4:out.write(ff+",代码行/空行/注释行："+t1+"/"+t2+"/"+t3+"\r\n");break;
+					default:
+					}
 				}
 			}
-		}
-        out.close();
+			out.close();
 	}
 	
-/*	public static boolean readfile(String filepath) throws FileNotFoundException, IOException {//读取文件或文件夹
-        try {
-
-                File file = new File(filepath);
-                if (!file.isDirectory()) {
-                        System.out.println("文件");
-                        System.out.println("path=" + file.getPath());
-                        System.out.println("absolutepath=" + file.getAbsolutePath());
-                        System.out.println("name=" + file.getName());
-
-                } else if (file.isDirectory()) {
-                        System.out.println("文件夹");
-                        String[] filelist = file.list();
-                        for (int i = 0; i < filelist.length; i++) {
-                                File readfile = new File(filepath + "\\" + filelist[i]);
-                                if (!readfile.isDirectory()) {
-                                        System.out.println("path=" + readfile.getPath());
-                                        System.out.println("absolutepath="
-                                                        + readfile.getAbsolutePath());
-                                        System.out.println("name=" + readfile.getName());
-
-                                } else if (readfile.isDirectory()) {
-                                        readfile(filepath + "\\" + filelist[i]);
-                                }
-                        }
-
-                }
-
-        } catch (FileNotFoundException e) {
-                System.out.println("readfile()   Exception:" + e.getMessage());
-        }
-        return true;
-}*/
+	public static void hard(String f) throws IOException{
+		File ff=new File(f);
+		InputStreamReader fff=new InputStreamReader(new FileInputStream(f));
+		BufferedReader freader=new BufferedReader(fff);
+		String l=null;
+		int isflag=0;
+			while((l=freader.readLine())!=null){
+				isflag=0;
+				for(int i=0;i<l.length();i++){
+					if(l.charAt(i)!=' '&&l.charAt(i)!='/'&&isflag==0&&l.charAt(i)!='\t')
+						{isflag=1;continue;}
+					if(l.charAt(i)!=' '&&l.charAt(i)!='/'&&isflag==1)
+						{isflag=10;break;}
+					if(l.charAt(i)=='/'&&isflag==0)
+						{isflag=2;continue;}
+					if(l.charAt(i)=='/'&&isflag==1)
+						{isflag=2;continue;}
+					if(l.charAt(i)=='/'&&isflag==2)
+						{isflag=11;break;}
+					
+				}
+				if(isflag==10||isflag==1)
+					t1++;
+				else if(isflag==11)
+					t3++;
+				else
+					t2++;
+			}
+			System.out.println(f+",代码行/空行/注释行："+t1+"/"+t2+"/"+t3);
+		
+	}
 	
 	public static void main(String[] args) throws IOException{
-		int[] flags=new int[]{0,0,0};
+		int[] flags=new int[]{0,0,0,0,0};
 		for(int i=0;i<args.length;i++){
 			if(args[i].equals("-c"))
 				flags[0]=1;
@@ -109,16 +178,45 @@ public class WorldCount {
 				flags[1]=1;
 			if(args[i].equals("-l"))
 				flags[2]=1;
-			if(!args[i].equals("-c")&&!args[i].equals("-w")&&!args[i].equals("-l")&&!args[i].equals("-o")){
-				fname=args[i];
+			if(args[i].equals("-a"))
+				flags[4]=1;
+			if(!args[i].equals("-c")&&!args[i].equals("-w")&&!args[i].equals("-l")&&!args[i].equals("-o")&&!args[i].equals("-a")&&!args[i].equals("-e")){
+				fname.add(args[i]);
 			}
 			if(args[i].equals("-o")){
 				outname=args[++i];
 			}
+			if(args[i].equals("-e")){
+				stopList=args[++i];
+				flags[3]=1;
+			}
 		}
-		wc(fname,flags);
-		if(outname!=null)
-			oout(outname,flags);
+		if(outname!=null){
+			File file = new File(outname);
+			if(!file.exists()){
+			       try {
+			            if(file.createNewFile()){
+			             }else{
+			                 System.out.println("文件创建失败！");
+			             }
+			         } catch (IOException e) {
+			               e.printStackTrace();
+			         }
+			}
+			else{
+				FileWriter out = new FileWriter(file);
+				out.write("");
+				out.close();
+			}
+		}
+		for(int i=0;i<fname.size();i++){
+			init();
+			wc(fname.get(i),flags);
+			if(flags[4]==1)
+				hard(fname.get(i));
+			if(outname!=null)
+				oout(outname,fname.get(i),flags);
+		}
 		return;
 	}
 }
